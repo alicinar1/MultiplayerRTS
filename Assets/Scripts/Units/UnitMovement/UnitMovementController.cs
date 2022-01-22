e39287585f17ffd4ac8b5b4ef646402f4eae5af0
alicinar1 <alicinar1@users.noreply.github.com>
2022-01-22 23:59:05 +0300

    Unit movement

---
+++ Assets/Scripts/Units/UnitMovement/UnitMovementController.cs
@@ -0,0 +1,59 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class UnitMovementController : NetworkBehaviour
+{
+    [SerializeField] private NavMeshAgent _player;
+    private Camera mainCamera;
+
+
+    #region Server
+
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas))
+        {
+            return;
+        }
+        _player.SetDestination(position);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        if (!Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            return;
+        }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            return;
+        }
+
+        CmdMove(hit.point);
+    }
+    #endregion
+}
