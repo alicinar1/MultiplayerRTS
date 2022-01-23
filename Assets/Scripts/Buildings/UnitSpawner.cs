585c22da62f96904400cf7dd57de42cec6ff3564
alicinar1 <alicinar1@users.noreply.github.com>
2022-01-23 23:01:35 +0300

    SpawningUnits

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject _unitPrefab = null;
+    [SerializeField] private Transform _spawnPoint;
+
+
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(_unitPrefab, _spawnPoint.position, _spawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [SerializeField] private GameObject _unitSpawner;
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+        GameObject unitSpawnerInstance = Instantiate(_unitSpawner, conn.identity.transform.position, conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
