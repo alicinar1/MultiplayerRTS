939647ce272ace4ace02feda8a6b9e42c3136249
alicinar1 <alicinar1@users.noreply.github.com>
2022-01-23 23:37:05 +0300

    SelectingUnits

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,41 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent _onSelected = null;
+    [SerializeField] private UnityEvent _onDeselected = null;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        _onSelected?.Invoke();
+        Debug.Log("Selected");
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        _onDeselected?.Invoke();
+    } 
+
+    #endregion
+
+
+
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask _layerMask = new LayerMask();
+    private Camera mainCamera;
+    private List<Unit> selectedUnits = new List<Unit>();
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            foreach (Unit selectedUnit in selectedUnits)
+            {
+                selectedUnit.Deselect();
+            }
+
+            selectedUnits.Clear();
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+            Debug.Log("MouseReleased");
+        }
+    }
+
+    private void ClearSelectionArea()
+    {
+        Debug.Log("Clear Selection Area");
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _layerMask))
+        {
+            Debug.Log("Return !Physics");
+            return;
+        }
+
+        if (!hit.collider.TryGetComponent<Unit>(out Unit unit))
+        {
+            Debug.Log("Return !hit");
+            return;
+        }
+
+        if (!unit.hasAuthority)
+        {
+            Debug.Log("Return !unit");
+            return;
+        }
+
+        selectedUnits.Add(unit);
+        Debug.Log(selectedUnits.Count);
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+            Debug.Log(selectedUnit.name);
+        }
+    }
+}
