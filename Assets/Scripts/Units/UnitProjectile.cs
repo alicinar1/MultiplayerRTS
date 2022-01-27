14ceb000ebdac34f7c5ca1455da258190afcbfbe
alicinar1 <muhammeda.cinar@gmail.com>
2022-01-27 22:59:30 +0300

    Dealing Damage

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -6,12 +6,32 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
     private void Start()
     {
         rb.velocity = transform.forward * launchForce;
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient)
+            {
+                return;
+            }
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     public override void OnStartServer()
